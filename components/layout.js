359bfa58faed6b9aeeb92acd437e6032a1284262
Troy Sisson <tasisson12@gmail.com>
2023-09-14 15:58:23 -0600

    Revert change

--- components/layout.js
+++ components/layout.js
@@ -18,7 +18,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="../site.webmanifest" />
         <meta
-          name="description"
+          name="troy.sisson"
           content="Personal website"
         />
         <meta
